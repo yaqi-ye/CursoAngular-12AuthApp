c1692bfc6b94b1fd4cb586b40ee5bf36d33cf104
Yaqi <yaqiye22@gmail.com_>
2023-04-04 18:17:01 +0200

    Sección 26. 04_04_2023 wip.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 const { dbConnection } = require('./database/config');
 require( 'dotenv' ).config();
 
@@ -24,6 +26,11 @@ app.use( express.json() );
 
 // Rutas.
 app.use( '/api/auth', require( './routes/auth' ) );
+
+// Manejar todas las posibles rutas.
+app.get( '*', ( req, res ) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ) )
+});
 
 app.listen( process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
